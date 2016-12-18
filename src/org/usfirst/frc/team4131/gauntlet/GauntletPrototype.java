e15957c4820c8c80ce16bae0385b90b23798f46f
endreman0 <endreman@squidgey.com>
2016-12-18 15:27:45 -0800

    First commit

---
+++ src/org/usfirst/frc/team4131/gauntlet/GauntletPrototype.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4131.gauntlet;
+
+/**
+ * Prototype client for the gesture recognition system.
+ * @author endreman0
+ */
+public class GauntletPrototype{
+	public static void main(String[] args){
+		
+	}
+}
